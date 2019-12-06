43ea24cd6f7facb4101481824a7fccc22cd26cee
UlyC <36838240+UlyC@users.noreply.github.com>
2019-12-06 10:45:25 +0800

    add segment mode,support prefix

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './style.css';
+import styles from './style.css';
 
 /**
  * Function to get cursor position
  */
 function getRange(el) {
     var cuRange, tbRange, headRange, range, dupRange, ret = {};
-    if(el.setSelectionRange){
+    if (el.setSelectionRange) {
         // standard
         ret.begin = el.selectionStart;
         ret.end = el.selectionEnd;
         ret.result = el.value.substring(ret.begin, ret.end);
-    }else if(document.selection){
+    } else if (document.selection) {
         // ie
-        if(el.tagName.toLowerCase() === 'input'){
+        if (el.tagName.toLowerCase() === 'input') {
             cuRange = document.selection.createRange();
             tbRange = el.createTextRange();
             tbRange.collapse(true);
@@ -25,7 +25,7 @@ function getRange(el) {
             ret.end = headRange.text.length;
             ret.result = cuRange.text;
             cuRange.select();
-        }else if(el.tagName.toLowerCase() === 'textarea'){
+        } else if (el.tagName.toLowerCase() === 'textarea') {
             range = document.selection.createRange();
             dupRange = range.duplicate();
             dupRange.moveToElementText(el);
@@ -40,109 +40,153 @@ function getRange(el) {
 }
 
 class IPut extends React.Component {
-  /**
-   * set default props
-   */
-  static defaultProps = {
-    className: '',
-    defaultValue: '...',
-    isError: () => false,
-    onChange: new Function()
-  }
-  /**
-   * set prop type
-   */
-  static propTypes = {
-    className: PropTypes.string,
-    defaultValue: PropTypes.string || PropTypes.array,
-    isError: PropTypes.func,
-    onChange: PropTypes.func
-  }
-  /**
-   * default state value
-   */
-  state = {
-    value: []
-  }
-  /**
-   * constructor
-   */
-  constructor() {
-    super();
-  }
-  componentDidMount() {
-    this.setState({
-      value: Array.isArray(this.props.defaultValue) ? this.props.defaultValue : this.props.defaultValue.split('.')
-    });
-  }
-  /**
-   * Change Event
-   */
-  handleChange(e, i) {
-    let val = parseInt( e.target.value );
-    if( isNaN(e.target.value) ) { return e.preventDefault(); }
-    if( val !== '' && (val > 255 || val < 0) ) { val = 255; }
+    /**
+     * set default props
+     */
+    static defaultProps = {
+        className: '',
+        defaultValue: '...',
+        isError: () => false,
+        onChange: new Function()
+    };
+    /**
+     * set prop type
+     */
+    static propTypes = {
+        className: PropTypes.string,
+        defaultValue: PropTypes.string || PropTypes.array,
+        isError: PropTypes.func,
+        onChange: PropTypes.func
+    };
+    /**
+     * default state value
+     */
+    state = {
+        value: [],
+        segment:27,
+    };
 
-    let value = this.state.value;
-    value[i] = val;
-    this.setState({value}, () => this.onPropsChange());
-
-    if( !isNaN(val) && String(val).length === 3 && i < 3 ) { this[`_input-${i + 1}`].focus(); }
-  }
-  /**
-   * Keydown Event
-   */
-  handleKeyDown(e, i) {
-    /* 37 = ←, 39 = →, 8 = backspace */
-    let domId = i;
-    if( (e.keyCode === 37 || e.keyCode === 8) && getRange(e.target).end === 0 && i > 0 ) { domId = i - 1; }
-    if( e.keyCode === 39 && getRange(e.target).end === e.target.value.length && i < 3 ) { domId = i + 1; }
-    this[`_input-${domId}`].focus();
-  }
-  /**
-   * Paste Event
-   */
-  handlePaste(e, i) {
-    let value = e.clipboardData.getData('text/plain').split('.').map(val => parseInt(val));
-    if( value.length === 4 - i && value.every(val => !isNaN(val) && val >= 0 && val <= 255) ) {
-      let oldValue = this.state.value;
-      value.forEach( (val, j) => oldValue[i+j] = val );
-      this.setState({value: oldValue}, () => this.onPropsChange());
-      return e.preventDefault();
+    /**
+     * constructor
+     */
+    constructor() {
+        super();
     }
-  }
-  /**
-   * call change props
-   */
-  onPropsChange() {
-    this.props.onChange( this.state.value.map(val => isNaN(val) ? '' : val).join('.') );
-  }
 
-  render() {
-    let className = [
-      'react-ip-input',
-      this.props.className,
-      this.props.isError() ? 'has-error' : ''
-    ].join(' ');
+    componentDidMount() {
+        this.setState({
+            value: Array.isArray(this.props.defaultValue) ? this.props.defaultValue : this.props.defaultValue.split('.')
+        });
+    }
 
-    return (
-      <div className={className}>
-        {this.state.value.map( (val, i) =>
-          <div className="react-ip-input__item" key={i}>
-            <input
-                ref={(el) => this[`_input-${i}`] = el}
-                type="text"
-                defaultValue={isNaN(val) ? '' : val}
-                value={isNaN(val) ? '' : val}
-                onChange={(e) => this.handleChange(e, i)}
-                onKeyDown={(e) => this.handleKeyDown(e, i)}
-                onPaste={(e) => this.handlePaste(e, i)}
-            />
-            {i !== 3 ? <i>.</i> : false}
-          </div>
-        )}
-      </div>
-    );
-  }
+    /**
+     * Change Event
+     */
+    handleChange(e, i) {
+        let val = parseInt(e.target.value);
+        if (isNaN(e.target.value)) {
+            return e.preventDefault();
+        }
+        if (val !== '' && (val > 255 || val < 0)) {
+            val = 255;
+        }
+
+        let value = this.state.value;
+        value[i] = val;
+        this.setState({value}, () => this.onPropsChange());
+
+        if (!isNaN(val) && String(val).length === 3 && i < 3) {
+            this[`_input-${i + 1}`].focus();
+        }
+    }
+
+    /**
+     * Keydown Event
+     */
+    handleKeyDown(e, i) {
+        /* 37 = ←, 39 = →, 8 = backspace */
+        let domId = i;
+        if ((e.keyCode === 37 || e.keyCode === 8) && getRange(e.target).end === 0 && i > 0) {
+            domId = i - 1;
+        }
+        if (e.keyCode === 39 && getRange(e.target).end === e.target.value.length && i < 3) {
+            domId = i + 1;
+        }
+        this[`_input-${domId}`].focus();
+    }
+
+    /**
+     * Paste Event
+     */
+    handlePaste(e, i) {
+        let value = e.clipboardData.getData('text/plain').split('.').map(val => parseInt(val));
+        if (value.length === 4 - i && value.every(val => !isNaN(val) && val >= 0 && val <= 255)) {
+            let oldValue = this.state.value;
+            value.forEach((val, j) => oldValue[i + j] = val);
+            this.setState({value: oldValue}, () => this.onPropsChange());
+            return e.preventDefault();
+        }
+    }
+
+    /**
+     * call change props
+     */
+    onPropsChange() {
+        const {mode} = this.props;
+        if (mode==="segment"){
+            this.props.onChange(this.state.value.map(val => isNaN(val) ? '' : val).join('.')+"/"+this.state.segment);
+        }else {
+            this.props.onChange(this.state.value.map(val => isNaN(val) ? '' : val).join('.'));
+        }
+    }
+
+    render() {
+        const {prefix, mode} = this.props;
+        let className = [
+            'react-ip-input',
+            this.props.className,
+            this.props.isError() ? 'has-error' : ''
+        ].join(' ');
+
+        const prefixInput = prefix ?
+            <input className="prefix" style={{width: prefix.length * 40}} type="text" disabled
+                   defaultValue={prefix}/> : "";
+
+        const segment = mode === "segment" ?
+            <div className="input-group" >
+                <input className="prefix" disabled type="text" value={"/"} style={{width: 25}} />
+                <input className="segment" type="number"
+                   max={32} min={0} step={2}
+                   onChange={(e) => this.setState({segment:e.target.value},() => this.onPropsChange())}
+                   defaultValue={27}/>
+            </div>
+            :
+            "";
+
+
+        return (
+            <div className={"div"}>
+                {prefixInput}
+                <div className={className}>
+                    {this.state.value.map((val, i) =>
+                        <div className="react-ip-input__item" key={i}>
+                            <input
+                                ref={(el) => this[`_input-${i}`] = el}
+                                type="text"
+                                defaultValue={isNaN(val) ? '' : val}
+                                value={isNaN(val) ? '' : val}
+                                onChange={(e) => this.handleChange(e, i)}
+                                onKeyDown={(e) => this.handleKeyDown(e, i)}
+                                onPaste={(e) => this.handlePaste(e, i)}
+                            />
+                            {i !== 3 ? <i>.</i> : false}
+                        </div>
+                    )}
+                </div>
+                {segment}
+            </div>
+        );
+    }
 }
+
 export default IPut;
